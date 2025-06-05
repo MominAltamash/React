a6df26a1a25a4b3868a2c0573da5e8d23356d836
MominAltamash <mominaltamash37@gmail.com>
2025-06-05 15:45:20 +0530

    bg color

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from "react"
 
 function App() {
 
-  const [color, setColor] = useState('#000')
-setColor()
+  const [color, setColor] = useState('#000');
   return (
     <>
-      <div className="w-full h-full" style={{backgroundColor: color}}>
+      <div className="w-full h-screen" style={{backgroundColor: color}}>
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-3">
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-            Test Buttons
+            <button className="px-2 py-1 rounded ouline-0" onClick={() => setColor('red')} style={{backgroundColor: "red"}}>Red</button>
+            <button className="px-2 py-1 rounded ouline-0" onClick={() => setColor('green')} style={{backgroundColor: "green"}}>Green</button>
+            <button className="px-2 py-1 rounded ouline-0" onClick={() => setColor('blue')} style={{backgroundColor: "blue"}}>Blue</button>
           </div>
         </div>
       </div>
