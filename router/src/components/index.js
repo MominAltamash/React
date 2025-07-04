9634583eee201bf1757e0a95be91833c4c9e4631
MominAltamash <mominaltamash37@gmail.com>
2025-07-04 12:15:47 +0530

    commit

--- router/src/components/index.js
+++ router/src/components/index.js
@@ -2,5 +2,7 @@ import Header from './Header/Header'
 import Home from './Home/Home'
 import About from './About/About'
 import Footer from './footer/footer'
+import Contact from './Contact/Contact'
+import User from './User/User'
 
-export {Header, Home, About, Footer}
+export {Header, Home, About, Footer, Contact, User}
--- router/src/main.jsx
+++ router/src/main.jsx
@@ -2,26 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
-import { About, Home } from './components/index.js'
+import { About, Contact, Home, User } from './components/index.js'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "about",
-        element: <About />
-      },
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       },
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/user/:userid' element={<User />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
