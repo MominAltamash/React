13ef9f2a4e550cbc613df3efd938ed5a012d9370
MominAltamash <mominaltamash37@gmail.com>
2025-07-08 12:35:35 +0530

    update

--- contaxtAPI/src/components/Login.jsx
+++ contaxtAPI/src/components/Login.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import UserContext from '../context/UserContext'
 
 function Login() {
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleSubmit = () => {
+   const {setUser} = useContext(UserContext)
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setUser({username, password})
     }
     return (
         <div>
