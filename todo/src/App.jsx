a58b5e74bb91f3ab25788dc3d363bf42d58b7fdd
MominAltamash <mominaltamash37@gmail.com>
2025-07-09 12:35:29 +0530

    New Todo Project

---
+++ todo/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-cyan-600 p-4 text-center text-white'>Todo App Using ContextAPI and Local Storage</h1>
+    </>
+  )
+}
+
+export default App
