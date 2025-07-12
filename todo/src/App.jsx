ba14ebb37897fe4317bb421f92eeb838dce7d7e6
MominAltamash <mominaltamash37@gmail.com>
2025-07-12 18:54:35 +0530

    todo

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,12 +1,31 @@
 
+import { useState } from 'react'
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
 
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {}
+  const updateTodo = () => {}
+  const deleteTodo = () => {}
+  const toggleComplete = () => {}
+
   return (
-    <>
-      <h1 className='text-3xl bg-cyan-600 p-4 text-center text-white'>Todo App Using ContextAPI and Local Storage</h1>
-    </>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}> 
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */} 
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
   )
 }
 
